3c3ac1d5620b63f43656c6b3a675aa1655099897
HadrienMP <public@hadrienmp.fr>
2017-07-31 12:37:53 +0200

    En tant que joueur je veux lancer une partie #2 - Retours de code review

--- src/main/resources/webapp/scripts/app.js
+++ src/main/resources/webapp/scripts/app.js
@@ -1,19 +1,19 @@
 $(function(){
     $.get({
-    	url: 'http://localhost:8080/player/id',
-    	success: function(data){
-            		$('#player-id').text(data);
-        		},
-    	error: function(){
+    	'url' : 'http://localhost:8080/player/id',
+    	'success' : function(data){
+				$('#player-id').text(data);
+			},
+    	'error' : function(){
     		alert('La partie a déjà commencé');
     	}
     });
     	
-    $('#start-game').click(function(){
+    $('#start-game').on('click', function(){
     	$.post({
-    		url: 'http://localhost:8080/game',
-    	success : function(data){
-    		$('#start-game').hide();
+    		'url' : 'http://localhost:8080/game',
+			'success' : function(data){
+				$('#start-game').hide();
     		}
     	});
     });
